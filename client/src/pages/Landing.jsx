82f22820ef0f6f3c71f1699d195767816b0b2016
rickytsangdev <rickytsang.dev@gmail.com>
2024-07-20 16:26:54 +0200

    start styled component for Landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,22 @@
+import styled from "styled-components";
+
 const Landing = () => {
-	return <div>Landing page</div>;
+	return (
+		<Wrapper>
+			<h1>Landing page</h1>
+			<div className="content">Some content</div>
+		</Wrapper>
+	);
 };
 
+const Wrapper = styled.div`
+	background-color: red;
+  h1 {
+    color: white;
+  }
+  .content {
+    background: blue;
+    color: yellow;
+  }
+`;
 export default Landing;
